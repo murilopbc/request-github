bafad8dc64b1ea208c4d695242e66c51effd3dc3
murilopbc <murilopoltronieri3@gmail.com>
2024-05-02 16:29:39 -0300

    commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+username = input('Qual é o nome do usuário? ')
+
+url = f'https://api.github.com/users/{username}'
+
+response = requests.get(url)
+data = response.json()
+
+if response.status_code == 200:
+    print(f'\nNome completo: {data["name"]}')
+    print(f'Bio: {data["bio"]}')
+    print(f'Localização: {data["location"]}')
+    print(f'Seguidores: {data["followers"]}')
+    print(f'Seguindo: {data["following"]}')
+else:
+    print('Não foi possível encontrar o usuário!')
